3b514856cfeaadca4c4e28d775afbde244b50b95
Andras Szerdahelyi <andras.szerdahelyi@gmail.com>
2015-08-31 18:59:10 +0200

    Allow parameters to configure.rb to come from CONFIGURE_PARAMS as opposed to stdin

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 
-ruby /root/configure.rb $*
-supervisord -c /etc/supervisord.conf
+# Allow parameters to configure.rb to come from CONFIGURE_PARAMS as opposed to stdin
+# This is to support parameterizing via Compose in particular. The two lines below are equivalent in effect:
+# docker run -e "CONFIGURE_PARAMS=cassandra --dc daas-1 --calculate-tokens 1:1" -ti --name c1 --rm andlaz/hadoop-cassandra
+# docker run -ti --name c1 --rm andlaz/hadoop-cassandra cassandra --dc daas-1 --calculate-tokens 1:1
+P=${CONFIGURE_PARAMS:-$(echo $*)}
+set -- $P
+
+case $1 in
+	help) ruby /root/configure.rb $* ;;
+	*) ruby /root/configure.rb $* && supervisord -c /etc/supervisord.conf
+esac
